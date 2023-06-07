eaa79f07dc57272fbc9f934c57a618e54f3fc8af
youngsoon12 <enejwk789@gmail.com>
2023-06-07 16:47:25 +0900

    add: server 구현중

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+const OpenAI = require('openai');
+const {Configuration, OpenAIApi} = OpenAI;
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 3001;
 
+const configuration = new Configuration({
+  organization: 'org-YOkfC0Bq9ZRbOXgPughUasaO',
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/', (req, res) => {
-  res.send({
-    message: 'Hello world',
+app.post('/', async (req, res) => {
+  const {message} = req.body;
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${message}`,
+    max_tokens: 4000,
+    temperature: 0,
   });
+  console.log(response.data);
+  if (response.data) {
+    if (response.data.choices) {
+      res.json({
+        message: response.data.choices[0].text,
+      });
+    }
+  }
 });
 
 app.listen(port, () => {
